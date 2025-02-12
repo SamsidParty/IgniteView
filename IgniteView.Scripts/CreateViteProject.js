e13eb744d0bb060389d370ac2f0d7fd8f52d5112
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-02-12 19:43:07 +0300

    Tooling: Add vite project creation
    Co-Authored-By: SamsidParty <business@samsidparty.com>

---
+++ IgniteView.Scripts/CreateViteProject.js
@@ -0,0 +1,41 @@
+const readline = require('readline');
+const spawnSync = require('child_process').spawnSync;
+const fs = require('fs');
+const path = require('path');
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function CreateViteProject() {
+    console.clear();
+    spawnSync(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['create', 'vite@latest', "./"], { stdio: 'inherit' });
+}
+
+function InstallDependencies() {
+    console.clear();
+    spawnSync(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', ['i'], { stdio: 'inherit' });
+}
+
+async function Main() {
+    console.log("\n-------- IgniteView \x1b[96mVite\x1b[0m Wizard --------\n");
+    process.chdir(path.join(process.cwd(), "src-vite"));
+    console.log("Working Directory: " + process.cwd() + "\n");
+
+    await new Promise((r) => interface.question("Press \x1b[92m\x1b[1menter\x1b[0m to initialize \x1b[96mVite\x1b[0m with this IgniteView project: ", r));
+
+    while (!fs.existsSync('./package.json')) {
+        CreateViteProject();
+    }
+
+    console.log("Created \x1b[96mVite\x1b[0m Project, installing dependencies...");
+
+    InstallDependencies();
+
+    console.log("Installed npm dependencies, continuing with IgniteView prebuild...");
+
+    process.exit(0);
+}
+
+Main();
--- IgniteView.Scripts/Prebuild.js
+++ IgniteView.Scripts/Prebuild.js
@@ -1,8 +1,45 @@
-const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const exec = require('child_process').exec;
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const scriptsURL = process.argv[1]; // $(ScriptsURL)
+const projectDirectory = process.argv[2]; // $(MSBuildProjectDirectory)
+process.chdir(projectDirectory);
 
-rl.question("Press enter to continue...", () => {});
+function CreateViteProject() {
+    console.log("No vite project found. Creating a new one...");
+    return new Promise((resolve, reject) => {
+        var prefix = "start \"\" cmd.exe /C ";
+        exec(prefix + `node -e "fetch('${scriptsURL}/CreateViteProject.js').then((c) => c.text().then(eval))"`, (_, stdout, __) => { resolve(stdout); });
+    });
+}
+
+async function PrebuildVite() {
+    console.log("Detected Project Type: Vite");
+    var sourcePath = path.join(projectDirectory, 'src-vite');
+
+    if (!fs.existsSync(sourcePath)) {
+        fs.mkdirSync(sourcePath);
+    }
+
+    var packageJsonPath = path.join(sourcePath, 'package.json');
+    
+    if (!fs.existsSync(packageJsonPath)) {
+        await CreateViteProject();
+    }
+}
+
+async function Main() {
+
+    console.log("\n-------- IgniteView Prebuild Version 0.0.1 --------\n");
+
+    // Determine the project type
+    if (fs.existsSync(path.join(projectDirectory, 'wwwroot'))) {
+        console.log("Detected Project Type: Static HTML");
+    }
+    else {
+        await PrebuildVite();
+    }
+}
+
+Main();
