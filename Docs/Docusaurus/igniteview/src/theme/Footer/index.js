9b68599b003ece42dedb2ce7b74d6ee746bf5726
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-02-20 08:49:53 +0300

    Docs: Create docusaurus project
    Co-Authored-By: SamsidParty <business@samsidparty.com>

---
+++ Docs/Docusaurus/igniteview/src/theme/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Footer from '@theme-original/Footer';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <iframe style={{ height: "27rem" }} src="https://samsidparty.com/embed/footer"></iframe>
+    </>
+  );
+}
