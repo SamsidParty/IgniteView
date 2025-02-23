0c718cbc2e6b116d320c02e7f3c6fe83cc80f223
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-02-23 18:11:32 +0300

    Docs: Start documenting command bridge
    Co-Authored-By: SamsidParty <business@samsidparty.com>

---
+++ Docs/docs/interop/call_csharp_from_js.md
@@ -0,0 +1,5 @@
+# Calling C# From JavaScript
+
+## Defining Commands
+
+Much like Tauri, IgniteView allows you to expose functions as "Commands", which can be called by JavaScript code.
---
+++ IgniteView.Example.Desktop/TestCommands.cs
@@ -0,0 +1,18 @@
+﻿using IgniteView.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgniteView.Example.Desktop
+{
+    class TestCommands
+    {
+        [Command("beep")]
+        private void BeepBeep()
+        {
+            Console.Beep();
+        }
+    }
+}
