b63b7f95c26e7d98e5e1b915aaae34dbe8548c7b
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-07-19 11:25:49 +0300

    Core: Fix commands not working with object parameters
    Co-Authored-By: SamsidParty <business@samsidparty.com>

--- IgniteView.Core/Managers/CommandManager.cs
+++ IgniteView.Core/Managers/CommandManager.cs
@@ -14,7 +14,8 @@ namespace IgniteView.Core
         {
             get
             {
-                if (_Commands == null) {
+                if (_Commands == null)
+                {
                     _Commands = new Dictionary<string, MethodInfo>();
 
                     // Find all commands through reflection
@@ -54,73 +55,90 @@ namespace IgniteView.Core
 
         public static async Task<object> ExecuteCommand(WebWindow? target, CommandData commandData)
         {
-            if (!Commands.ContainsKey(commandData.Function)) {
-                target?.ExecuteJavaScript(new JSFunctionCall("console.error", $"The command bridge couldn't locate a binding for command '{commandData.Function}', please ensure the command is correct."));
+            try
+            {
+                if (!Commands.ContainsKey(commandData.Function))
+                {
+                    target?.ExecuteJavaScript(new JSFunctionCall("console.error", $"The command bridge couldn't locate a binding for command '{commandData.Function}', please ensure the command is correct."));
+                    return null;
+                }
+
+                var method = Commands[commandData.Function]; // Find the MethodInfo of the command
+
+                // Run the method
+                var paramList = new List<object>();
+
+                var paramIndex = 0;
+                foreach (var parameter in method.GetParameters())
+                {
+                    if (parameter.ParameterType == typeof(WebWindow))
+                    {
+                        paramList.Add(target); // Inject the target WebWindow as a parameter
+                    }
+                    else
+                    {
+                        if (commandData.Parameters.Length <= paramIndex)
+                        {
+                            target?.ExecuteJavaScript(new JSFunctionCall("console.error", $"The command bridge couldn't transform parameter {paramIndex + 1} '{parameter.Name}' for command '{commandData.Function}', make sure you have passed this parameter when calling the function"));
+                            return null;
+                        }
+
+                        var providedParam = commandData.Parameters[paramIndex];
+                        var providedParamType = providedParam.GetType();
+                        var expectedParamType = parameter.ParameterType;
+
+                        if (providedParamType != expectedParamType)
+                        {
+                            // Try to automatically convert types
+                            var recoveredFromTypeError = false;
+
+                            if (expectedParamType == typeof(string))
+                            { // Easy conversion
+                                recoveredFromTypeError = true;
+                                providedParam = providedParam.ToString();
+                            }
+                            else if (expectedParamType == typeof(Int32) && providedParamType == typeof(Int64)) // Common error cause JS numbers are int64
+                            {
+                                recoveredFromTypeError = true;
+                                providedParam = Convert.ToInt32(providedParam);
+                            }
+                            else if (expectedParamType == typeof(object))
+                            {
+                                // No need to do anything
+                                recoveredFromTypeError = true;
+                            }
+
+                            if (!recoveredFromTypeError)
+                            {
+                                // Cannot automatically convert types, throw an error
+                                throw new ArgumentException("The provided parameter of type " + providedParamType.Name + " does not match expected type " + expectedParamType.Name);
+                            }
+
+                        }
+
+                        paramList.Add(providedParam);
+                        paramIndex++;
+                    }
+                }
+
+                var result = method.Invoke(null, paramList.ToArray());
+
+                // Handle async commands
+                if (result is Task)
+                {
+                    await (Task)result;
+                    result = ((dynamic)result).Result;
+                }
+
+                var returnFunction = new JSFunctionCall("window.igniteView.commandQueue.resolve", commandData.CallbackID, result);
+                target?.ExecuteJavaScript(returnFunction);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                target?.ExecuteJavaScript(new JSFunctionCall("console.error", $"The command bridge encountered an error while executing the command '{commandData.Function}': {ex.Message}"));
                 return null;
             }
-
-            var method = Commands[commandData.Function]; // Find the MethodInfo of the command
-
-            // Run the method
-            var paramList = new List<object>();
-
-            var paramIndex = 0;
-            foreach (var parameter in method.GetParameters()) {
-                if (parameter.ParameterType == typeof(WebWindow)) { 
-                    paramList.Add(target); // Inject the target WebWindow as a parameter
-                }
-                else
-                {
-                    if (commandData.Parameters.Length <= paramIndex)
-                    {
-                        target?.ExecuteJavaScript(new JSFunctionCall("console.error", $"The command bridge couldn't transform parameter {paramIndex + 1} '{parameter.Name}' for command '{commandData.Function}', make sure you have passed this parameter when calling the function"));
-                        return null;
-                    }
-
-                    var providedParam = commandData.Parameters[paramIndex];
-                    var providedParamType = providedParam.GetType();
-                    var expectedParamType = parameter.ParameterType;
-
-                    if (providedParamType != expectedParamType)
-                    {
-                        // Try to automatically convert types
-                        var recoveredFromTypeError = false;
-
-                        if (expectedParamType == typeof(string)) { // Easy conversion
-                            recoveredFromTypeError = true;
-                            providedParam = providedParam.ToString();
-                        }
-                        else if (expectedParamType == typeof(Int32) && providedParamType == typeof(Int64)) // Common error cause JS numbers are int64
-                        {
-                            recoveredFromTypeError = true;
-                            providedParam = Convert.ToInt32(providedParam);
-                        }
-
-                        if (!recoveredFromTypeError)
-                        {
-                            // Cannot automatically convert types, throw an error
-                            throw new ArgumentException("The provided parameter of type " + providedParamType.Name + " does not match expected type " + expectedParamType.Name);
-                        }
-
-                    }
-
-                    paramList.Add(providedParam);
-                    paramIndex++;
-                }
-            }
-
-            var result = method.Invoke(null, paramList.ToArray());
-
-            // Handle async commands
-            if (result is Task)
-            {
-                await (Task)result;
-                result = ((dynamic)result).Result;
-            }
-
-            var returnFunction = new JSFunctionCall("window.igniteView.commandQueue.resolve", commandData.CallbackID, result);
-            target?.ExecuteJavaScript(returnFunction);
-            return result;
         }
     }
 }
