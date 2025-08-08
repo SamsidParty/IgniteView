a9ffa1933c711b4c01eee2c423fe47285592e0a8
Samarth Pradeep <79756986+SamarthMP@users.noreply.github.com>
2025-08-08 12:26:06 +0300

    Core: Complete localStorage implementation

--- IgniteView.Core/Managers/AppManager.cs
+++ IgniteView.Core/Managers/AppManager.cs
@@ -80,7 +80,7 @@ namespace IgniteView.Core
             // Hydrate data
             RegisterPreloadScriptFromString(new JSFunctionCall("window.igniteView.commandBridge.fillCommandList", (object)InteropCommands.ListCommands()));
             RegisterPreloadScriptFromString(new JSAssignment("window.igniteView.platformHints", PlatformManager.PlatformHints));
-            RegisterPreloadScriptFromFunction(() => new JSAssignment("window._localStorageCache", LocalStorage.GetAllItems()));
+            RegisterPreloadScriptFromFunction(() => new JSFunctionCall("window._localStorage.hydrate", LocalStorage.GetAllItems()));
 
             // Register routes
             RegisterDynamicFileRoute("/streamedCommand", DynamicRoutes.StreamedCommandRoute);
--- IgniteView.Core/src-vite/src/localStorage.js
+++ IgniteView.Core/src-vite/src/localStorage.js
@@ -1,30 +1,63 @@
-if (!window._localStorageCache) {
-    window._localStorageCache = {};
-}
+if (!window._localStorage) {
+    class VirtualLocalStorage {
 
-window._localStorage = {
-    getItem: (item) => {
-        return window._localStorageCache[item] || null;
-    },
-    setItem: (item, value) => {
-        window._localStorageCache[item] = value;
-        window.igniteView?.commandBridge?.igniteview_localstorage_set(item, value.toString());
-        return value;
-    },
-    removeItem: (item) => {
-        var value = getItem(item);
-        delete window._localStorageCache[item];
-        window.igniteView?.commandBridge?.igniteview_localstorage_remove(item);
-        return value;
-    },
-    clear: () => {
-        window._localStorageCache = {};
-        window.igniteView?.commandBridge?.igniteview_localstorage_clear();
-    },
-    hydrate: async () => {
-        window._localStorageCache = await window.igniteView?.commandBridge?.igniteview_localstorage_getAllItems();
-        return window._localStorageCache;
+        _cache = {};
+
+        constructor() {
+            return new Proxy(this, {
+                set: function(target, property, value) {
+                    target.setItem(property, value);
+                    return true; 
+                },
+                get: function(target, property) {
+                    if (property in target) {
+                        return target[property];
+                    }
+
+                    return target.getItem(property);
+                }
+            });
+        }
+
+        getItem(item) {
+            return this._cache[item] || null;
+        }
+
+        setItem(item, value) {
+            this._cache[item] = value;
+
+            window.igniteView?.commandBridge?.igniteview_localstorage_set(item, value.toString());
+            return value;
+        }
+
+        removeItem(item) {
+            var value = this.getItem(item);
+            delete this._cache[item];
+            delete this[item];
+            window.igniteView?.commandBridge?.igniteview_localstorage_remove(item);
+            return value;
+        }
+
+        clear() {
+            this._cache = {};
+            window.igniteView?.commandBridge?.igniteview_localstorage_clear();
+            window._localStorage = new VirtualLocalStorage();
+            this.hydrate({});
+            delete this;
+        }
+
+        // Called by C#
+        hydrate(values) {
+            Object.entries(values).forEach(([key, value]) => this.setItem(key, value));
+            console.log("[IgniteView][LocalStorage] Hydrated");
+            try {
+                Object.defineProperty(window, "localStorage", { value: window._localStorage })
+            }
+            catch {}
+        }
     }
+
+    window._localStorage = new VirtualLocalStorage();
 }
 
 try {
